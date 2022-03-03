10dfcde45f25a0bfe86354306e1ed41d095dec4f
shykot75 <shykot1198@gmail.com>
2022-03-03 12:56:07 +0600

    Second Day of SMS Project

---
+++ day-32/resources/views/website/home/home.blade.php
@@ -0,0 +1,247 @@
+@extends('master.front.master')
+
+@section('body')
+
+    <section class="notice-section py-5">
+        <div class="container">
+            <div class="carousel slide" data-bs-ride="carousel" data-bs-interval="2000">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="card card-body rounded-0">
+                            <h3 class="card-title">Notice Title Goes Here dolor sit amet</h3>
+                            <hr>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae debitis deleniti dolore eveniet fuga id ipsa magnam neque officia, officiis, praesentium provident qui rem reprehenderit. Eum fugiat impedit voluptate!</p>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae debitis deleniti dolore eveniet fuga id ipsa magnam neque officia, officiis, praesentium provident qui rem reprehenderit. Eum fugiat impedit voluptate!</p>
+                        </div>
+                    </div>
+
+
+                    <div class="carousel-item">
+                        <div class="card card-body rounded-0">
+                            <h3 class="card-title">Notice Title Goes Here dolor sit amet</h3>
+                            <hr>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae debitis deleniti dolore eveniet fuga id ipsa magnam neque officia, officiis, praesentium provident qui rem reprehenderit. Eum fugiat impedit voluptate!</p>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae debitis deleniti dolore eveniet fuga id ipsa magnam neque officia, officiis, praesentium provident qui rem reprehenderit. Eum fugiat impedit voluptate!</p>
+                        </div>
+                    </div>
+
+                    <div class="carousel-item">
+                        <div class="card card-body rounded-0">
+                            <h3 class="card-title">Notice Title Goes Here dolor sit amet</h3>
+                            <hr>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae debitis deleniti dolore eveniet fuga id ipsa magnam neque officia, officiis, praesentium provident qui rem reprehenderit. Eum fugiat impedit voluptate!</p>
+                            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae debitis deleniti dolore eveniet fuga id ipsa magnam neque officia, officiis, praesentium provident qui rem reprehenderit. Eum fugiat impedit voluptate!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-6">
+                    <div class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-md-5">
+                                <img src="{{asset('/img/laravel.png')}}" class="img-fluid  w-100 h-100" alt="...">
+                            </div>
+                            <div class="col-md-7">
+                                <div class="card-body">
+
+                                    <figure>
+                                        <blockquote class="blockquote">
+                                            <p>A well-known quote, contained in a blockquote element.</p>
+                                        </blockquote>
+                                        <figcaption class="blockquote-footer">
+                                            Someone famous in <cite title="Source Title">Source Title</cite>
+                                        </figcaption>
+                                    </figure>
+
+
+                                    <ul>
+                                        <li>Course Feature One</li>
+                                        <li>Course Feature two</li>
+                                        <li>Course Feature Three</li>
+                                        <li>Course Feature Three</li>
+                                    </ul>
+
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <a href="{{route('course-detail')}}" class="btn btn-outline-info">Read More</a>
+                                    <a href="" class="btn btn-outline-success float-end">Apply Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-md-5">
+                                <img src="{{asset('/img/laravel2.png')}}" class="img-fluid  w-100 h-100" alt="...">
+                            </div>
+                            <div class="col-md-7">
+                                <div class="card-body">
+
+                                    <figure>
+                                        <blockquote class="blockquote">
+                                            <p>A well-known quote, contained in a blockquote element.</p>
+                                        </blockquote>
+                                        <figcaption class="blockquote-footer">
+                                            Someone famous in <cite title="Source Title">Source Title</cite>
+                                        </figcaption>
+                                    </figure>
+
+
+                                    <ul>
+                                        <li>Course Feature One</li>
+                                        <li>Course Feature two</li>
+                                        <li>Course Feature Three</li>
+                                        <li>Course Feature Three</li>
+                                    </ul>
+
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <a href="" class="btn btn-outline-info">Read More</a>
+                                    <a href="" class="btn btn-outline-success float-end">Apply Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-md-5">
+                                <img src="{{asset('/img/laravel2.png')}}" class="img-fluid  w-100 h-100" alt="...">
+                            </div>
+                            <div class="col-md-7">
+                                <div class="card-body">
+
+                                    <figure>
+                                        <blockquote class="blockquote">
+                                            <p>A well-known quote, contained in a blockquote element.</p>
+                                        </blockquote>
+                                        <figcaption class="blockquote-footer">
+                                            Someone famous in <cite title="Source Title">Source Title</cite>
+                                        </figcaption>
+                                    </figure>
+
+
+                                    <ul>
+                                        <li>Course Feature One</li>
+                                        <li>Course Feature two</li>
+                                        <li>Course Feature Three</li>
+                                        <li>Course Feature Three</li>
+                                    </ul>
+
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <a href="" class="btn btn-outline-info">Read More</a>
+                                    <a href="" class="btn btn-outline-success float-end">Apply Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="card mb-3">
+                        <div class="row g-0">
+                            <div class="col-md-5">
+                                <img src="{{asset('/img/laravel.png')}}" class="img-fluid  w-100 h-100" alt="...">
+                            </div>
+                            <div class="col-md-7">
+                                <div class="card-body">
+
+                                    <figure>
+                                        <blockquote class="blockquote">
+                                            <p>A well-known quote, contained in a blockquote element.</p>
+                                        </blockquote>
+                                        <figcaption class="blockquote-footer">
+                                            Someone famous in <cite title="Source Title">Source Title</cite>
+                                        </figcaption>
+                                    </figure>
+
+
+                                    <ul>
+                                        <li>Course Feature One</li>
+                                        <li>Course Feature two</li>
+                                        <li>Course Feature Three</li>
+                                        <li>Course Feature Three</li>
+                                    </ul>
+
+                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <a href="" class="btn btn-outline-info">Read More</a>
+                                    <a href="" class="btn btn-outline-success float-end">Apply Now</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <section class="py-5 bg-secondary">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card card-body ">
+                        <div class="row">
+                            <div class="col-9">
+                                <input type="text" name="" class="form-control"  placeholder="Enter Your Email to Subscribe">
+
+                            </div>
+                            <div class="col-3">
+                                <input type="submit" value="Subscribe Now" class="btn btn-outline-success w-100">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
