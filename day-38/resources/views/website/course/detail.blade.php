fd2e99d30df173da6783bae99347728f5b997e87
shykot75 <shykot1198@gmail.com>
2022-03-13 13:12:10 +0600

    project-1 day-3

---
+++ day-38/resources/views/website/course/detail.blade.php
@@ -0,0 +1,69 @@
+@extends('master.front.master')
+
+@section('title')
+    Course Detail Page
+@endsection
+
+
+@section('body')
+
+
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card-card-body">
+                        <img src="{{asset('/img/laravel2.png')}}" alt="" class="img-fluid w-100 h-auto">
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="card card-body">
+                        <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, totam.</h1>
+                        <p>Course Fee: 15999</p>
+                        <hr>
+                        <h3>Trainer Name</h3>
+                        <ul>
+                            <li><a href="#">Trainer LinkedIn Link</a></li>
+                            <li><a href="#">Trainer FaceBook Link</a></li>
+                            <li><a href="#">Trainer Twitter Link</a></li>
+                        </ul>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consectetur deleniti exercitationem in itaque magnam perspiciatis quia quibusdam reprehenderit tenetur?</p>
+
+                        <a href="" class="btn btn-outline-success">Enroll Now</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="card card-body">
+                        <h1>Course Module Details</h1>
+                        <hr>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi asperiores aspernatur aut deleniti dolorum eaque esse illo, incidunt laborum libero, neque officiis quaerat quasi qui, rerum sapiente unde voluptatibus?</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi asperiores aspernatur aut deleniti dolorum eaque esse illo, incidunt laborum libero, neque officiis quaerat quasi qui, rerum sapiente unde voluptatibus?</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi asperiores aspernatur aut deleniti dolorum eaque esse illo, incidunt laborum libero, neque officiis quaerat quasi qui, rerum sapiente unde voluptatibus?</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi asperiores aspernatur aut deleniti dolorum eaque esse illo, incidunt laborum libero, neque officiis quaerat quasi qui, rerum sapiente unde voluptatibus?</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi asperiores aspernatur aut deleniti dolorum eaque esse illo, incidunt laborum libero, neque officiis quaerat quasi qui, rerum sapiente unde voluptatibus?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+@endsection
