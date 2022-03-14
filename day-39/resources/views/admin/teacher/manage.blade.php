3a4500b16725828efda610326f8b27a7f8a3e5eb
shykot75 <shykot1198@gmail.com>
2022-03-14 13:15:01 +0600

    project-1 day-4

---
+++ day-39/resources/views/admin/teacher/manage.blade.php
@@ -0,0 +1,65 @@
+@extends('master.admin.master')
+
+@section('body')
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="card-title">Teachers tables</h4>
+                    <p class="card-title-desc">
+
+                    </p>
+
+                    <div class="table-responsive">
+                        <table class="table mb-0">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Table heading</th>
+                                <th>Table heading</th>
+                                <th>Table heading</th>
+                                <th>Table heading</th>
+                                <th>Table heading</th>
+                                <th>Table heading</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                                <td>Table cell</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
---
+++ day-39/routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WebController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TeacherController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [ WebController::class, 'index' ])->name('home');
+
+
+Route::get('/course-detail', [ WebController::class, 'detail' ])->name('course-detail');
+Route::get('/user-login', [ AuthController::class, 'login' ])->name('user-login');
+Route::get('/user-register', [ AuthController::class, 'register' ])->name('user-register');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', [ DashboardController::class, 'index' ])->name('dashboard');
+
+
+// ADMIN ADD MANAGE USER
+Route::middleware(['auth:sanctum', 'verified', 'superAdmin'])->get('/add-user', [ UserController::class, 'index' ])->name('add-user');
+Route::middleware(['auth:sanctum', 'verified', 'superAdmin'])->get('/manage-user', [ UserController::class, 'manage' ])->name('manage-user');
+Route::middleware(['auth:sanctum', 'verified', 'superAdmin'])->post('/new-user', [ UserController::class, 'create' ])->name('new-user');
+Route::middleware(['auth:sanctum', 'verified', 'superAdmin'])->get('/edit-user/{id}', [ UserController::class, 'edit' ])->name('edit-user');
+Route::middleware(['auth:sanctum', 'verified', 'superAdmin'])->get('/delete-user/{id}', [ UserController::class, 'delete' ])->name('delete-user');
+Route::middleware(['auth:sanctum', 'verified', 'superAdmin'])->post('/update-user/{id}', [ UserController::class, 'update' ])->name('update-user');
+
+
+
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/add-teacher', [ TeacherController::class, 'index' ])->name('add-teacher');
+Route::middleware(['auth:sanctum', 'verified'])->get('/manage-teacher', [ TeacherController::class, 'manage' ])->name('manage-teacher');
